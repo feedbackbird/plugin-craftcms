26907d4186f5f19f3436b2eb14b57fc00987c856
Mostafa Soufi <mostafa.soufi@hotmail.com>
2024-03-17 17:51:24 +0330

    Add option Opening Style

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,10 @@
 # Changelog
 
+## 1.0.2 - 2024-03-17
+- [Fix] Added option Opening Style
+
 ## 1.0.1 - 2024-03-09
-- [Fix] Correct load script data attribute. 
+- [Fix] Correct load script data attribute.
 
 ## 1.0.0 - 2024-02-20
 - Initial release
--- src/models/Settings.php
+++ src/models/Settings.php
@@ -11,6 +11,7 @@ class Settings extends Model
 {
     public $widgetStatus = true;
     public $widgetPosition = 'top-right';
+    public $widgetOpeningStyle = 'sides';
     public $widgetColor = '5562E0';
     public $widgetButtonLabel = '';
     public $widgetSubtitle = '';
@@ -22,6 +23,7 @@ class Settings extends Model
             [['widgetPosition', 'uID'], 'required'],
             ['widgetStatus', 'boolean', 'trueValue' => true, 'falseValue' => false],
             ['widgetPosition', 'in', 'range' => ['top-right', 'top-left', 'right', 'left', 'bottom-right', 'bottom-left']],
+            ['widgetOpeningStyle', 'in', 'range' => ['sides', 'modal']],
             ['widgetColor', 'string'],
             ['widgetButtonLabel', 'string'],
             ['widgetSubtitle', 'string'],
